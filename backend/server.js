be5e5260d5cbb338bc61c5908109cc7ea20a3348
mrtoukir8-1103 <mrtoukir8@gmail.com>
2025-12-30 20:41:28 +0530

    Set up Express server with CORS and JSON middleware

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/db");
 
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// DB Connection
+connectDB();
+
+// Routes
+app.use("/api/todos", require("./routes/todoRoutes"));
+
+// Server
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
